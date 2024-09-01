324d4680cef0d2927380849704623ea7ff85aead
Wagner_Moreira <61630676+wagnerCV@users.noreply.github.com>
2024-09-02 02:56:51 +0800

    Update blogData.tsx

--- src/components/Blog/blogData.tsx
+++ src/components/Blog/blogData.tsx
@@ -46,34 +46,3 @@ import { Blog } from "@/types/blog";
   },
 ];*/}
 
-const blogData: Blog[] = [
-  {
-    id: 1,
-    title: "Best UI components for modern websites",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-01.jpg",
-    tags: ["creative"],
-    publishDate: "2025",
-  },
-  {
-    id: 2,
-    title: "9 simple ways to improve your design skills",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-02.jpg",
-
-    tags: ["computer"],
-    publishDate: "2025",
-  },
-  {
-    id: 3,
-    title: "Tips to quickly improve your coding speed.",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-03.jpg",
-    tags: ["design"],
-    publishDate: "2025",
-  },
-];
-export default blogData;
