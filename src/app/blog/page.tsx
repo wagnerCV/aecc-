9350ea7da53264ac807b4ea3f823bfa8450acd97
Wagner_Moreira <61630676+wagnerCV@users.noreply.github.com>
2024-09-02 03:02:58 +0800

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,4 @@
 import SingleBlog from "@/components/Blog/SingleBlog";
-import blogData from "@/components/Blog/blogData";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 
 import { Metadata } from "next";
@@ -21,14 +20,6 @@ const Blog = () => {
       <section className="pb-[120px] pt-[120px]">
         <div className="container">
           <div className="-mx-4 flex flex-wrap justify-center">
-            {blogData.map((blog) => (
-              <div
-                key={blog.id}
-                className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
-              >
-                <SingleBlog blog={blog} />
-              </div>
-            ))}
           </div>
 
           <div className="-mx-4 flex flex-wrap" data-wow-delay=".15s">
