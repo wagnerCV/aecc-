090dd524af889f11969ae6ec255e787e82ec1b17
Wagner_Moreira <61630676+wagnerCV@users.noreply.github.com>
2024-09-02 03:08:12 +0800

    Update index.tsx

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,6 +1,6 @@
 import SectionTitle from "../Common/SectionTitle";
 import SingleBlog from "./SingleBlog";
-import blogData from "./blogData";
+
 
 const Blog = () => {
   return (
@@ -16,11 +16,6 @@ const Blog = () => {
         />
 
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
-          {blogData.map((blog) => (
-            <div key={blog.id} className="w-full">
-              <SingleBlog blog={blog} />
-            </div>
-          ))}
         </div>
       </div>
     </section>
