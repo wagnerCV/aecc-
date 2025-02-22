834e14cabca03904da0e8415e6eb57791771affc
Wagner_Moreira <61630676+wagnerCV@users.noreply.github.com>
2025-02-23 05:42:25 +0800

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,17 @@
 "use client";
+import Head from 'next/head';
 
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
+    <>
+    <Head>
+        <meta name="google-adsense-account" content="ca-pub-1767079045992000" />
+      </Head>
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
       {children}
     </ThemeProvider>
+  </>
   );
 }
