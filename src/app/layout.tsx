68bee85846ee8282ab16cc18889dea6654aaf86e
Wagner_Moreira <61630676+wagnerCV@users.noreply.github.com>
2025-02-23 05:18:09 +0800

    Update layout.tsx
    add corrections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Head from 'next/head';
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
 import "../styles/index.css";
@@ -28,9 +29,11 @@ export default function RootLayout({
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       
-      <head>
-      <link rel="icon" href="favicon1.png" /> 
-        <meta name="google-adsense-account" content="ca-pub-1767079045992000">
+      <Head>
+        <link rel="icon" href="favicon1.png" /> 
+        <meta name="google-adsense-account" content="ca-pub-1767079045992000" />
+      </Head>
+
       </head>
 
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
